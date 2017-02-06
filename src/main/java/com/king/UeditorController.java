093672412a8170463655c19f02c74ba1f20e600f
ZG <zgms59@hotmail.com>
2017-02-06 21:35:22 +0800

    优化

--- src/main/java/com/king/UeditorController.java
+++ src/main/java/com/king/UeditorController.java
@@ -21,7 +21,7 @@ public class UeditorController {
     @RequestMapping(method = RequestMethod.GET)
     public void getConfig(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String url = UeditorController.class.getResource("/ueditor-config.json").getPath();
-        Path path = Paths.get(url);
+        Path path = Paths.get(url.substring(1, url.length()));
         List<String> lines = Files.readAllLines(path);
         StringBuilder sb = new StringBuilder();
         lines.forEach(line -> sb.append(line));
